509c5fc97eedc204adb29f2f752efb06ccc4ccaa
satya-reddy-123 <sunnyreddy0411@gmail.com>
2024-09-24 12:20:21 +0530

    Add files via upload

---
+++ comparetor.ino
@@ -0,0 +1,26 @@
+void setup() {
+ Serial.begin(9600);
+ int a,b,c,d,e,f,g;
+ a=5,b=7;
+ if (a==b)
+{
+  Serial.println("a is equal to b");
+
+}
+else{
+  Serial.println("a is not equal to b");
+
+}
+if (a>b)
+{
+  Serial.println("a is greater than b");
+
+}
+else {
+  Serial.println("a is less than b");
+  
+}
+}
+
+void loop() {
+}
